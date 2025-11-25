c2f0246d073b7bd87e4ff43e1783093eab3179f8
Ubuntu <ubuntu@ip-172-31-28-172.ap-northeast-1.compute.internal>
2025-11-25 00:40:49 +0000

    [Finish book index]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
-
+  validates :introduction, length: { maximum: 50 }
+  has_one_attached :image
 
   def email_required?
     false
