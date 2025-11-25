7dfe3d43f123f7d0bb223bfc7ada0e6bf03d5a1e
Ubuntu <ubuntu@ip-172-31-28-172.ap-northeast-1.compute.internal>
2025-11-25 01:21:54 +0000

    [Finish create]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,30 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @books = Book.all
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id  
+  
+  if @book.save
+    redirect_to book_path(@book)
+  else
+    @user = current_user
+    @books = Book.all
+    render :index 
+  end
+end
 
   def show
   end
 
   def edit
+  end
+
+    private 
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
